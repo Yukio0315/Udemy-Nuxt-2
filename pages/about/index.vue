2669fd6d64309bbff691fbe4fb6690f8b4050613
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2020-01-01 13:26:00 +0900

    created base of pages

---
+++ pages/about/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+      urna. Aliquam pharetra erat quam, bibendum volutpat dolor finibus sed.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Pellentesque in odio vitae lectus tincidunt tempus.
+      Suspendisse faucibus ullamcorper lacus, eu luctus ipsum. Etiam maximus,
+      dui a dictum commodo, ipsum est pharetra elit, at viverra felis velit
+      dictum nunc. In vel sem id nisl fringilla ornare eu vel mauris. Nullam
+      hendrerit turpis et felis posuere convallis egestas in nisi. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Nulla id posuere tortor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+      urna. Aliquam pharetra erat quam, bibendum volutpat dolor finibus sed.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Pellentesque in odio vitae lectus tincidunt tempus.
+      Suspendisse faucibus ullamcorper lacus, eu luctus ipsum. Etiam maximus,
+      dui a dictum commodo, ipsum est pharetra elit, at viverra felis velit
+      dictum nunc. In vel sem id nisl fringilla ornare eu vel mauris. Nullam
+      hendrerit turpis et felis posuere convallis egestas in nisi. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Nulla id posuere tortor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+      urna. Aliquam pharetra erat quam, bibendum volutpat dolor finibus sed.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Pellentesque in odio vitae lectus tincidunt tempus.
+      Suspendisse faucibus ullamcorper lacus, eu luctus ipsum. Etiam maximus,
+      dui a dictum commodo, ipsum est pharetra elit, at viverra felis velit
+      dictum nunc. In vel sem id nisl fringilla ornare eu vel mauris. Nullam
+      hendrerit turpis et felis posuere convallis egestas in nisi. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Nulla id posuere tortor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+      urna. Aliquam pharetra erat quam, bibendum volutpat dolor finibus sed.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Pellentesque in odio vitae lectus tincidunt tempus.
+      Suspendisse faucibus ullamcorper lacus, eu luctus ipsum. Etiam maximus,
+      dui a dictum commodo, ipsum est pharetra elit, at viverra felis velit
+      dictum nunc. In vel sem id nisl fringilla ornare eu vel mauris. Nullam
+      hendrerit turpis et felis posuere convallis egestas in nisi. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Nulla id posuere tortor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+      urna. Aliquam pharetra erat quam, bibendum volutpat dolor finibus sed.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Pellentesque in odio vitae lectus tincidunt tempus.
+      Suspendisse faucibus ullamcorper lacus, eu luctus ipsum. Etiam maximus,
+      dui a dictum commodo, ipsum est pharetra elit, at viverra felis velit
+      dictum nunc. In vel sem id nisl fringilla ornare eu vel mauris. Nullam
+      hendrerit turpis et felis posuere convallis egestas in nisi. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Nulla id posuere tortor.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo
+      urna. Aliquam pharetra erat quam, bibendum volutpat dolor finibus sed.
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+      inceptos himenaeos. Pellentesque in odio vitae lectus tincidunt tempus.
+      Suspendisse faucibus ullamcorper lacus, eu luctus ipsum. Etiam maximus,
+      dui a dictum commodo, ipsum est pharetra elit, at viverra felis velit
+      dictum nunc. In vel sem id nisl fringilla ornare eu vel mauris. Nullam
+      hendrerit turpis et felis posuere convallis egestas in nisi. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Nulla id posuere tortor.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
